c6dc4d0a1a34ebcb95157eebed9a6dfe297e62ba
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-16 13:54:52 -0800

    0x00-hello_world add #101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: print without printf or puts
+ * Return: Always  1 (Error)
+ */
+int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(0, s, 59);
+	return (1);
+}
