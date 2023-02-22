5be87fcbfde438b22bf4bf9f1f2d4879ad78b88e
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-22 09:44:57 -0800

    0x02-functions_nested_loops: add #4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - Check if the character is alphabet
+ * @c: The character recieved as an integer
+ * Return: 1 (True) 0 (False)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
+}
