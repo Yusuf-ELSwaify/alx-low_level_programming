bb0a0c2811b4f3981c4cae4d6eba46631ac10a42
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-22 11:06:18 -0800

    0x02-functions_nested_loops: add #6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Turns the negative value to postive
+ * @n: Integer value postive or negative
+ * Return: Only postive integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+
+	return (n);
+}
