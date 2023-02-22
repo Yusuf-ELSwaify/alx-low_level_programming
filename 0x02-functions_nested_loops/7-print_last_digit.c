7ce689402bf5c5101f6544df350daf752f6ed358
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-22 11:11:30 -0800

    0x02-functions_nested_loops: add #7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - get the last digit in a number
+ * @n: a number value
+ * Return: the last digit in a number
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
