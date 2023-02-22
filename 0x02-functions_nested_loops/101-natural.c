7029677ee83a92a6c4a3f8de025482c354c63090
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-22 15:38:28 -0800

    0x02-functions_nested_loops add #101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * sum_of_multiples - calculate the sum of the all the natural number
+ * that multiple to the integer multiple and below the integer below
+ * @multiple: the number multiplied by
+ * @below: number we stoped at
+ * Return: the sum of the numbers
+ */
+int sum_of_multiples(int multiple, int below)
+{
+	int i = 1, sum = 0;
+
+	while (i * multiple < below)
+	{
+		sum += i++ * multiple;
+	}
+	return (sum);
+}
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	const int below = 1024;
+	int multiple3 = sum_of_multiples(3, below);
+	int multiple5 = sum_of_multiples(5, below);
+
+	printf("%d\n", multiple3 + multiple5);
+	return (0);
+}
