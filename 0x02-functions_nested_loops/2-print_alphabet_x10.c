d6c4b7a1b18ebfce90ce75b283abb23864c13729
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-22 08:04:30 -0800

    0x02-functions_nested_loops: add #2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet - print chars from a to z 10 times
+ * print_alphabet_x10 - print chars from a to z 10 times
+ *
  * Return: void
  */
 void print_alphabet_x10(void)
 {
 	int i = 0, c;
+
 	while (i++ < 10)
-	{	
+	{
 		c = 'a';
 
 		while (c <= 'z')
 			_putchar(c++);
+
 		_putchar('\n');
 	}
 }
