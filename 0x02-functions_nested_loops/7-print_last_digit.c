da74e04d09e193ae2a6a2c82800fc7a3d957d6cb
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-22 11:56:34 -0800

    0x02-functions_nested_loops: edit #7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,9 @@
  */
 int print_last_digit(int n)
 {
-	int remainder = (n < 0 ? -n : n) % 10; 
+	int remainder = n % 10;
+
+	remainder = remainder < 0 ? -remainder : remainder;
 	_putchar('0' + remainder);
 	return (remainder);
 }
