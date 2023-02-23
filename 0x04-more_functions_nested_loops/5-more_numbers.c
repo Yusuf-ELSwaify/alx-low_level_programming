ccfa89a5a9f43ca55f53b38fbcea58475f669eda
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-23 13:06:52 -0800

    0x04-more_functions_nested_loops: add #5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - prints a number character character
+ * @num: postive number
+ * Return: void
+ */
+void print_number(int num)
+{
+	if (num > 9)
+		print_number(num / 10);
+	_putchar('0' + num % 10);
+}
+/**
+ * more_numbers - print numbers from 0 to 14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0, num = 0;
+
+	while (i++ < 10)
+	{
+		while (num <= 14)
+			print_number(num++);
+
+		_putchar('\n');
+	}
+}
