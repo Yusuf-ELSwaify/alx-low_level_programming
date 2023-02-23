0ac30776cb6939077fe3cb127745b43d73554959
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-23 15:34:12 -0800

    0x04-more_functions_nested_loops: add #9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * print_number - prints a number character character
+ * @num: postive number
+ * Return: void
+ */
+void print_number(int num)
+{
+	if (num > 9)
+		print_number(num / 10);
+	putchar('0' + num % 10);
+}
+/**
+ * main - print numbers from 0 to 100 10 if a number
+ * multiples 3 print Fizz if five print Buzz if both print FizzBuzz
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i++ < 100)
+	{
+		if (i % 3 == 0)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+		}
+		if (i % 5 == 0)
+		{
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+		}
+		else if (i % 3 != 0)
+		{
+			print_number(i);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
