9a6196d379c53cfb89bc1ce5dae77f9fb0499d09
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-22 17:08:17 -0800

    0x02-functions_nested_loops add #102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,16 +2,34 @@
 /**
  * fibonacci - calculate the fibonacci of the number
  * @num: the number
+ * @fibonnaci_numbers: array collect in it the calculated
+ * fibonacci number to not recalculate it again
  * Return: the fibonnaci of a number
  */
-int fibonacci(int num)
+long fibonacci(int num, long fibonnaci_numbers[])
 {
-	if (num == 1)
-		return 1;
-	if (num == 2)
-		return 2;
+	long fnum, snum;
 
-	return fibonacci(num - 1) + fibonacci(num - 2);
+	if (num == 1)
+		return (1);
+	if (num == 2)
+		return (2);
+
+	if (fibonnaci_numbers[num - 1] != 0)
+		fnum = fibonnaci_numbers[num - 1];
+	else
+	{
+		fnum = fibonacci(num - 1, fibonnaci_numbers);
+		fibonnaci_numbers[num - 1] = fnum;
+	}
+	if (fibonnaci_numbers[num - 2] != 0)
+		snum = fibonnaci_numbers[num - 2];
+	else
+	{
+		snum =  fibonacci(num - 2, fibonnaci_numbers);
+		fibonnaci_numbers[num - 2] = snum;
+	}
+	return (fnum + snum);
 }
 /**
  * main - Entry point
@@ -19,8 +37,17 @@ int fibonacci(int num)
  */
 int main(void)
 {
+	long fibonnaci_numbers[53] = {0};
 	int i = 0;
-	while (i < 52)
-		printf("%d\n", fibonacci(++i));
+
+	while (1)
+	{
+		printf("%ld", fibonacci(++i, fibonnaci_numbers));
+		if (i == 50)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
 	return (0);
 }
