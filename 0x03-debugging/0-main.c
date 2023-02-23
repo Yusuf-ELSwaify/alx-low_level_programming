6b0476f54aceff6640ae1bb65b6f62891ec99e04
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-23 07:54:29 -0800

    0x03-debugging: add #1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
@@ -10,5 +9,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
