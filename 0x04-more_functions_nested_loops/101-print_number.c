05d28cfa59079ed4f04e303360977f57fb0a25be
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-23 17:24:54 -0800

    0x04-more_functions_nested_loops: add #101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,13 @@
  * Return: void
  */
 void print_number(int num)
-{
+{	unsigned int unum = num;
 	if (num < 0)
 	{
 		_putchar('-');
-		print_number(-num);
-		return;
+		unum = -num;
 	}
-	if (num > 9)
-		print_number(num / 10);
-	_putchar('0' + num % 10);
+	if (unum > 9)
+		print_number(unum / 10);
+	_putchar('0' + unum % 10);
 }
