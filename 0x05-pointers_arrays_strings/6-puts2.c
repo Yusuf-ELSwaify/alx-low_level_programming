029661ad9c47a3622997adb46151e9ea40da0cac
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-27 12:39:23 -0800

    0x05-pointers_arrays_strings add #6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints the even character in the string followed by a new line
+ * @str: pointer to array of characters
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
