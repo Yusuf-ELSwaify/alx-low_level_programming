2a18f1028c67af570b44d6c2c572096837e92f4a
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-27 12:25:55 -0800

    0x05-pointers_arrays_strings add #5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverse the string
+ * @s: pointer to array of characters
+ */
+void rev_string(char *s)
+{
+	int start = 0, end = 0;
+	char temp;
+
+	while (s[end] != '\0')
+		end++;
+	end--;
+	while (start != end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
