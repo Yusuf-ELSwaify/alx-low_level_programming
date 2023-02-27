296729a3e5fab0d5c57b7349ff6c6053a163920c
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-27 11:01:17 -0800

    0x05-pointers_arrays_strings add #3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - print the string followed by a new line
+ * @str: pointer to array of characters
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		_putchar(s[i++]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 int _putchar(char);
 void reset_to_98(int *);
 int _strlen(char *s);
+void _puts(char *str);
