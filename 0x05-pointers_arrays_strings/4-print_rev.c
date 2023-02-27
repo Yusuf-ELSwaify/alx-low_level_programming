82dde7e135acc9e9f03fa5d1775f1f85e6d30a5a
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-27 11:49:14 -0800

    0x05-pointers_arrays_strings add #4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,10 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i++] != '\0')
-		;
+	while (s[i] != '\0')
+		i++;	;
 
-	while (--i != 0)
+	while (i-- != 0)
 		_putchar(s[i]);
 	_putchar('\n');
 }
