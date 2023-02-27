0b7553bda13b900dbd721497c13b276c3c3015e1
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-27 15:57:43 -0800

    0x05-pointers_arrays_strings add #8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	if (n < 0)
+	if (n <= 0)
 		return;
 	while (1)
 	{
 		printf("%d", a[i++]);
-		if (i > n)
+		if (i == n)
 			break;
 		printf(", ");
 	}
