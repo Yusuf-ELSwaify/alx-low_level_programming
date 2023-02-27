8602ab7fd212a03bb22c228f209927bd884a1bfc
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-02-27 11:37:32 -0800

    0x05-pointers_arrays_strings add #4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints the string in reverse followed by a new line
+ * @s: pointer to array of characters
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i++] != '\0')
+		;
+
+	while (--i != 0)
+		_putchar(s[i]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ int _putchar(char);
 void reset_to_98(int *);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
