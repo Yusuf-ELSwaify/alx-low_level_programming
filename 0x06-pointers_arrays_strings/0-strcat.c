e45fdc59939c4e428023e2c4b65246f599469258
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-02 08:43:56 -0800

    0x06-pointers_arrays_strings add #0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,13 +4,13 @@
  * @src: the string copy from it
  * Return: pointer to dest
  */
-char * _strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
 	while (dest[i++] != 0)
 		;
-	while (src[j] != 0)
+	while (src[j - 1] != 0)
 	{
 		dest[i++] = src[j++];
 	}
