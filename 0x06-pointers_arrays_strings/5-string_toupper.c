4129decc6fb2c7466ea551c34fadb4eb5e8cd82c
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-02 15:24:58 -0800

    0x06-pointers_arrays_strings add #5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,7 +8,7 @@ char *string_toupper(char *s)
 	const int diff_between_a_A = ('a' - 'A');
 	int i = -1;
 
-	while (s[i++] != 0)
+	while (s[++i] != 0)
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= diff_between_a_A;
 	return (s);
