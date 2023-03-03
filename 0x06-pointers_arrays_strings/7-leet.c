3c20a42ab659065c13212eca7ef080e831e809f2
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-02 18:03:30 -0800

    0x06-pointers_arrays_strings edit #7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,12 +5,12 @@
  */
 char *leet(char *s)
 {
-	int i = -1;
+	int i = -1, j;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (s[++i] != 0)
 	{
-		int j = 0;
+		j = 0;
 		while (j <= 7)
 		{
 			if (s[i] == leet[j] || s[i] - 32 == leet[j])
