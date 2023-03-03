3b28a88acce63c0da276812aa443db0f5f8645aa
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-02 19:58:06 -0800

    0x06-pointers_arrays_strings add #103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,17 +4,20 @@
  * @n2: 2nd string
  * @r: the result
  * @size_r: r size
+ * Return: the sum r if it not exceed size_r else 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i = 1, len1 = 0, len2 = 0, carry = 0, max_length;
 
-	while (n1[len1] != '\0') len1++;
-	while (n2[len2] != '\0') len2++;
+	while (n1[len1++] != '\0')
+		;
+	while (n2[len2++] != '\0')
+		;
 	max_length = len1 > len2 ? len1 : len2;
 
 	if (max_length > size_r - 1)
-		return 0;
+		return (0);
 
 	r[max_length] = '\0';
 
@@ -32,13 +35,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (carry > 0)
 	{
 		if (max_length + 1 > size_r)
-			return 0;
+			return (0);
 		i = max_length + 1;
-		while (i > 0) {
+		while (i > 0)
+		{
 			r[i] = r[i - 1];
 			i--;
 		}
 		r[0] = carry + '0';
 	}
-	return r;
+	return (r);
 }
