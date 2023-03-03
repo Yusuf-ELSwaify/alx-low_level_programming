7af12512256c6dcf4466d007dabb02a60d39eec4
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-03 06:46:40 -0800

    0x06-pointers_arrays_strings add #104

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * print_buffer - print a buffer
+ * @b: string
+ * @size:  bytes of the buffer pointed by b
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0, j;
+
+	while (i < size)
+	{
+		printf("%08x:", i);
+		j = i;
+		while (j - i < 10)
+		{
+
+			if (j >= size)
+				printf(" %4s", "");
+			else if (j + 1 >= size)
+				printf(" %02x%2s", b[j], "");
+			else
+				printf(" %02x%02x", b[j], b[j + 1]);
+
+			j += 2;
+		}
+		putchar(' ');
+		j = i - 1;
+		while (++j - i < 10)
+			putchar((b[j] >= 31 && b[j] <= 126) ? b[j] 
+					: (j < size) ? '.' : ' ');
+		putchar('\n');
+		i += 10;
+	}
+
+}
