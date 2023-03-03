ac939e90c3a149b814eaeb1eb8df3d21c61104eb
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-02 17:31:21 -0800

    0x06-pointers_arrays_strings add #100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,18 @@
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: the string to encode
+ * Return: pointer to encoded s
+ */
+char *rot13(char *s)
+{
+	int i = -1;
+
+	while (s[++i] != 0)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z' - 13) || (s[i] >= 'A' && s[i] <= 'Z' - 13))
+			s[i] += 13;
+		else if ((s[i] > 'z' - 13 && s[i] <= 'z') || (s[i] > 'Z' - 13 && s[i] <= 'Z'))
+			s[i] -= 13;
+	}
+	return (s);
+}
