401452a3ccf6f952e2165891f5deb03beaa1ccb5
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-06 01:31:40 -0800

    0x07-pointers_arrays_strings add #0:#3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,16 @@
 /**
  * _strchr - locates a character in a string
  * @s: string to search in
- * @b: character to search for
+ * @c: character to search for
  * Return: pointer to the first occurence of c in s
  */
 char *_strchr(char *s, char c)
 {
-	while (*++s != c)
-		;
+	while (*s)
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
 	return (s);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -21,7 +21,7 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 			if (accept[i] == *s)
-			{	
+			{
 				segment_bytes++;
 				break;
 			}
