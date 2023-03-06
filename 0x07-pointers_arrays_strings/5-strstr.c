92e20084895d851b98648ea9f91360f8a9a7c257
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-06 10:07:58 -0800

    0x07-pointers_arrays_strings# edit #5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 /**
- * _strpbrk -  function locates the first occurrence in the string s
- * of any of the bytes in the string accept
- * @s: string to search in
- * @accept: bytes to accept if the string contain any of it
- * Return: pointer to the first occurence
- * of any character of accept in s
+ * _strstr -  function locates the first occurrence in the string haystack
+ * of string accept
+ * @haystack: string to search in
+ * @needle: bytes to accept if the string contain it
+ * Return: pointer to the first occurence of needle in haystack
  */
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, ii, jj;
 
-	while (haystack[i])
+	if (!*needle)
+		return (haystack);
+	while (haystack[i] != 0)
 	{
 		jj = 0, ii = i;
-		while (haystack[ii])
+		while (haystack[ii] && needle[jj] == haystack[ii])
 		{
-			if (needle[jj] != haystack[ii])
-				break;
 			if (!needle[jj + 1])
 				return (haystack + i);
 			ii++;
 			jj++;
 		}
 		i++;
-	}
-	return (0);
+	}printf("  sss \n");
+	return ('\0');
 }
