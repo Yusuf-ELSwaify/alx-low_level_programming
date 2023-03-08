1b1a9bd270080ff5ce33b233b3c56ac25bfb2f9b
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-08 10:28:55 -0800

    0x08-recursion add #101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,21 +1,3 @@
-int wildcmp(char *s1, char *s2);
-/**
- * wildcard - check when the string located at wildcard
- * @s1: string
- * @s2: string could contain *
- * Return: 1 if the string end 
- * otherwise pointer to first unmatched character
- */
-int wildcard(char *s1, char *s2)
-{
-	if (!*s1)
-		return (1);
-	if (*s2 == '*')
-		return (wildcard(s1, ++s2));
-	if (*s1 == *s2)
-		return (wildcmp(++s1, ++s2));
-	return (wildcard(++s1, s2));
-}
 /**
  * wildcmp - Compares two strings, considering wildcard characters
  * @s1: string to be compared
@@ -26,10 +8,13 @@ int wildcmp(char *s1, char *s2)
 {
 	if (!*s1 && !*s2)
 		return (1);
-	if (!*s1 || !*s2)
-		return (0);
 	if (*s2 == '*')
-		return (wildcard(++s1, ++s2));
+	{
+		if (!*s1 && *(s2 + 1)  && *s2 == '*')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
 	if (*s1 == *s2)
 		return (wildcmp(++s1, ++s2));
 	return (0);
