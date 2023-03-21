05bc8a43ced8f5933a4aefcd373e40ff1056732c
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-20 19:55:00 -0700

    0x0E-structures_typedef edit #2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,16 +1,20 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_dog - print variable of type dog
  * @d: object of type dog to be printed
  */
 void print_dog(struct dog *d)
 {
+	char c_age[20];
 	if (!d)
 		return;
-
+	sprintf(c_age, "%f",d->age);
 	printf("Name: %s\n"
-		"Age: %f\n"
+		"Age: %s\n"
 		"Owner: %s\n"
-			, d->name, d->age, d->owner);
+			, d->name ? d->name : "(nil)"
+			, d->age > 0 ? c_age : "(nil)"
+			, d->owner ? d->owner : "(nil)");
 }
