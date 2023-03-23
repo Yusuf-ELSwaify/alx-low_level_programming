2e02edbe02f7d8f9232a0236884631a99394c206
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-03-22 20:00:03 -0700

    0x0F-function_pointers add #100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the opcode
+ * @argc: number of parameters
+ * @argv: parameters
+ * Return: 0(success)
+ */
+int main(int argc, char **argv)
+{
+	int bytes, i = 0;
+	char *address = (char *)main;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+		printf("Error\n"), exit(2);
+
+	while (i <= bytes)
+		printf("%02hhx ", address[i++]);
+	putchar('\n');
+	return (0);
+}
