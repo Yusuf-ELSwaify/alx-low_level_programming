dea4e9dc00871bf8640284de9ba4545c05347196
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-04-03 10:47:37 -0700

    0x13-more_singly_linked_lists add #9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (idx)
+	while (idx > 1)
 	{
 		if (!temp)
 			return (NULL);
