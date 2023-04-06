77ace5db089664efb49f47fbe9002411cc987bb9
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-04-06 05:35:37 -0700

    0x14-bit_manipulation add #1 "print binary" - #2 "get bit"

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >>1);
+	printf("%lu", n & 1);
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_bit - get the value of a bit at a given index
+ * @n: number
+ * @index: index of bit
+ * Return: the value of a bit at a given index.
+ * 	If error return -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(n) * 8))
+		return (-1);
+	return ((n >> index) & 1);
+}
+
