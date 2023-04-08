ecbdbcd97747e17d60d72db571c5f9c3d963adfc
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-04-08 14:21:45 -0700

    0x15-file_io #1 "create file"

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - create a text file
+ * @filename: file name
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 (success)
+ *	if error -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, len = 0, w;
+
+	if (!filename)
+		return (-1);
+	while (text_content[len])
+		++len;
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	w = write(file, text_content, len);
+	if (w == -1)
+		return (-1);
+	close(file);
+	return (1);
+
+}
+
