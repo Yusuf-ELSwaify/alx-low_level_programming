902b725f7d468671148e6a10fe592a36a81f9127
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-05-18 19:46:32 -0700

    0x17-doubly_linked_lists add #8 solution after deleted by mistake

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes a node at index from linked list
+ * @head: address of the pointer of the head of double linked list
+ * @index: the index to delete the node
+ * Return: 1 if success, or -1 if failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *curr;
+
+	if (*head == NULL)
+		return (-1);
+
+	curr = *head;
+
+	while (curr && index--)
+		curr = curr->next;
+	if (!curr)
+		return (-1);
+
+	if (curr->prev != NULL)
+		curr->prev->next = curr->next;
+	else
+		*head = curr->next;
+
+	if (curr->next != NULL)
+		curr->next->prev = curr->prev;
+
+	return (1);
+}
