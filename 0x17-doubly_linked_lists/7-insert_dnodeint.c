4fc54ef634f5aeb18e34dec9f9d42001c6ab21aa
Yusuf EL-Swaify <yusufelswaify12@gmail.com>
2023-05-18 18:18:31 -0700

    0x17-doubly_linked_lists #7 handle index bigger than the list and index is the last node cases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,17 +13,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	new = malloc(sizeof(*new));
+	curr = *h;
+	
+    while (curr && --idx)
+		curr = curr->next;
+    if (!curr)
+        return (NULL);
+    if (curr->next == NULL)
+		return (add_dnodeint_end(&curr, n));
+	
+    new = malloc(sizeof(*new));
 	if (!new)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
 	new->next = *h;
-	if (!*h)
-		return (*h = new);
-	curr = *h;
-	while (curr->next && idx--)
-		curr = curr->next;
 	new->next = curr->next;
 	curr->next = new;
 	new->prev = curr;
